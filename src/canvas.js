5629beb152a384a0700c9f4f455e7ba9216ad327
Yugeta.Koji <geta1972@gmail.com>
2025-05-26 07:55:23 +0900

    コメント追加

--- src/canvas.js
+++ src/canvas.js
@@ -15,11 +15,11 @@ export class Canvas{
     font_style       : null,
     font_variant     : null,
   }
-  canvas = null
 
   constructor(options){
     if(!options || !options.text){return}
     this.options = Object.assign(this.setting, options)
+    // 文字表示幅のセット（指定がない場合は文字サイズの1.5倍）
     this.options.line_height = this.options.line_height || this.options.font_size * 1.5
     this.canvas = this.set_canvas(this.options.selector)
     this.set_canvas_size()
@@ -27,26 +27,39 @@ export class Canvas{
     this.set_event()
   }
 
+  // 表示処理（再描画でも利用する）
   view(){
     const long_lines = this.get_wrapper_lines()
     const short_lines = this.get_range_lines(long_lines)
-
     const lines = this.setting.height === "auto" ? long_lines : short_lines
     this.reset_canvas(lines)
     this.clear_canvas()
     this.draw_lines(lines)
   }
 
+
+  /**
+   * Event
+   */
+
+  // イベント設定
   set_event(){
     window.addEventListener("resize", this.resize_window.bind(this))
   }
 
+  // イベント処理 : windowリサイズ
   resize_window(){
     if(!this.canvas){return}
     this.set_canvas_size()
     this.view()
   }
 
+
+  /**
+   * Canvas
+   */
+
+  // 対象canvasの取得処理
   set_canvas(selector){
     const target = document.querySelector(selector)
     if(selector && target.tagName === "CANVAS"){
@@ -57,6 +70,7 @@ export class Canvas{
     }
   }
 
+  // canvasの描画をクリアする（再描画処理用）
   clear_canvas(){
     if(!this.canvas){return}
     this.canvas.getContext("2d").clearRect(
@@ -66,6 +80,7 @@ export class Canvas{
     )
   }
 
+  // 指定selectorがcanvasじゃない場合は、createする
   create_canvas_context(target) {
     if(!target){return}
     const canvas = document.createElement("canvas");
@@ -73,6 +88,7 @@ export class Canvas{
     return canvas
   }
 
+  // canvasサイズのセット（リサイズ事にセットし直す）
   set_canvas_size(){
     this.canvas.width  = this.canvas.offsetWidth
     this.canvas.height = this.canvas.offsetHeight
@@ -80,6 +96,7 @@ export class Canvas{
     this.ctx.font      = this.get_font()
   }
 
+  // フォント設定も文字列作成
   get_font(){
     let fonts = []
     if(this.setting.font_style){
@@ -95,6 +112,26 @@ export class Canvas{
     fonts.push(this.setting.font_family)
     return fonts.join(" ")
   }
+
+  // canvasの再描画に必要なheightリサイズや、フォント設定処理
+  reset_canvas(lines){
+    // ★ 必要な高さを計算して canvas を再設定（ここで高さ確定）
+    if(this.setting.height === "auto"){
+      this.canvas.height = lines.length * this.setting.line_height + (this.setting.padding * 2)
+    }
+    else if(this.canvas.height < this.canvas.offsetHeight){
+      this.canvas.height = lines.length * this.setting.line_height + (this.setting.padding * 2)
+    }
+    
+    // ★ フォントとスタイルを再設定（canvas 再生成後の初期化必須）
+    this.ctx.font      = this.get_font()
+    this.ctx.fillStyle = this.setting.text_color
+  }
+
+
+  /**
+   * Text
+   */
 
   // 行毎のテキストとx,y座標を取得
   get_wrapper_lines() {
@@ -123,6 +160,7 @@ export class Canvas{
         lines.push({
           text : sub_line,
           x    : this.setting.padding,
+          // 文字幅(line-height)の中間に文字が表示される仕様
           y    : this.setting.line_height * (lines.length + 1) + this.setting.padding - (this.setting.line_height - this.setting.font_size) / 2,
         })
       }
@@ -149,20 +187,7 @@ export class Canvas{
     return datas;
   }
 
-  reset_canvas(lines){
-    // ★ 必要な高さを計算して canvas を再設定（ここで高さ確定）
-    if(this.setting.height === "auto"){
-      this.canvas.height = lines.length * this.setting.line_height + (this.setting.padding * 2)
-    }
-    else if(this.canvas.height < this.canvas.offsetHeight){
-      this.canvas.height = lines.length * this.setting.line_height + (this.setting.padding * 2)
-    }
-    
-    // ★ フォントとスタイルを再設定（canvas 再生成後の初期化必須）
-    this.ctx.font      = this.get_font()
-    this.ctx.fillStyle = this.setting.text_color
-  }
-
+  // canvas内に文字描画する処理
   draw_lines(lines) {
     for(const line of lines){
       this.ctx.fillText(line.text, line.x, line.y)
