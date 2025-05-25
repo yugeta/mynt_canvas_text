2a6ade8fe64e301271001b099b1a6d8a6c8b6f29
Yugeta.Koji <geta1972@gmail.com>
2025-05-25 11:54:26 +0900

    resize x

--- src/canvas.js
+++ src/canvas.js
@@ -14,17 +14,33 @@ export class Canvas{
 
   constructor(options){
     this.options = Object.assign(this.setting, options)
-
     this.init()
+    this.set_event()
   }
   
   async init(){
     this.canvas = this.set_canvas(this.options.selector)
     if(!this.canvas){return}
-    const text  = await this.load_text()
-    const lines = this.get_wrapper_lines(text)
+    this.set_canvas_size()
+    this.text  = await this.load_text()
+    this.view()
+  }
+
+  view(){
+    const lines = this.get_wrapper_lines()
     this.reset_canvas(lines)
+    this.clear_canvas()
     this.draw_lines(lines)
+  }
+
+  set_event(){
+    window.addEventListener("resize", this.resize_window.bind(this))
+  }
+
+  resize_window(){
+    if(!this.canvas){return}
+    this.set_canvas_size()
+    this.view()
   }
 
   set_canvas(selector){
@@ -37,16 +53,27 @@ export class Canvas{
     }
   }
 
+  clear_canvas(){
+    if(!this.canvas){return}
+    this.canvas.getContext("2d").clearRect(
+      0, 0, 
+      this.canvas.width, 
+      this.canvas.height,
+    )
+  }
+
   create_canvas_context(target) {
     if(!target){return}
     const canvas = document.createElement("canvas");
-    // this.maxWidth = width - this.marginX * 2;
     target.appendChild(canvas);
-    canvas.width  = canvas.offsetWidth
-    canvas.height = canvas.offsetHeight
-    this.ctx      = canvas.getContext("2d")
-    this.ctx.font = this.setting.font_size + `px serif`
     return canvas
+  }
+
+  set_canvas_size(){
+    this.canvas.width  = this.canvas.offsetWidth
+    this.canvas.height = this.canvas.offsetHeight
+    this.ctx           = this.canvas.getContext("2d")
+    this.ctx.font      = this.setting.font_size + `px serif`
   }
 
   async load_text(){
@@ -58,28 +85,42 @@ export class Canvas{
     return res
   }
 
-  get_wrapper_lines(text) {
-    const max_width = this.canvas.width  - (this.setting.padding * 2)
-    const maxHeight = this.canvas.height - (this.setting.padding * 2)
-    const maxLines  = Math.floor(maxHeight / this.setting.line_height)
-    const lines = []
-    const paragraphs = text.split('\n');
+  // 行毎のテキストとx,y座標を取得
+  get_wrapper_lines() {
+    const text       = this.text
+    const max_width  = this.canvas.width  - (this.setting.padding * 2)
+    const maxHeight  = this.canvas.height - (this.setting.padding * 2)
+    const maxLines   = Math.floor(maxHeight / this.setting.line_height)
+    const lines      = []
+    const paragraphs = text.split('\n')
+    let num          = 1
     for (const paragraph of paragraphs) {
       let words = paragraph.split('')
-      let line = ''
-      for (let i = 0; i < words.length; i++) {
-        const testLine = line + words[i]
+      let line = ``
+      const sub_lines = []
+      let num_lines = 0
+      for (const word of words) {
+        sub_lines[num_lines] = sub_lines[num_lines] || ""
+        const testLine = sub_lines[num_lines] + word
         const width = this.ctx.measureText(testLine).width
-
         if (width > max_width && line !== '') {
-          lines.push(line)
-          line = words[i]
+          num_lines++
+          line = word
         } 
         else {
           line = testLine
         }
+        sub_lines[num_lines] = line
       }
-      if (line) lines.push(line)
+      for(const sub_line of sub_lines){
+        lines.push({
+          text : sub_line,
+          x : this.setting.padding,
+          y : this.setting.line_height * num + this.setting.padding,
+        })
+        num++
+        if(lines.length >= maxLines){break}
+      }
       if(lines.length >= maxLines){break}
     }
     return lines;
@@ -90,24 +131,14 @@ export class Canvas{
     if(this.canvas.height < this.canvas.offsetHeight){
       this.canvas.height = lines.length * this.setting.line_height + 20
     }
-
     // ★ フォントとスタイルを再設定（canvas 再生成後の初期化必須）
     this.ctx.font      = this.setting.font_size + `px serif`
     this.ctx.fillStyle = this.setting.text_color
   }
 
   draw_lines(lines) {
-    const x = this.setting.padding
-    let line_num = 0
-    lines.forEach((line, i) => {
-      const y = this.setting.line_height * (i + 1) + this.setting.padding
-      // const y = this.setting.font_size * (i + 1)
-      // if(line_num >= 5){
-      //   this.ctx.filter = `blur(5px)`
-      // }
-      line_num++
-      this.ctx.fillText(line, x, y)
-      // console.log(x,y,line)
-    })
+    for(const line of lines){
+      this.ctx.fillText(line.text, line.x, line.y)
+    }
   }
 }
